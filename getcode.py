e3f26282954c31e8f974ebdc0a8c094a6970bb99
malinowy5 <malinowy5@proton.me>
2024-03-16 13:17:58 +0100

    wszystko oprocz jumpow

--- getcode.py
+++ getcode.py
@@ -5,18 +5,6 @@ a=0; b=0; c=0; d=0; s=0; i=0; f=0
 # i - instruction pointer
 # f - flags
 
-opcodes = {
-			int("10", 16):"MOV",
- 			int("08", 16):"ADD", 
- 			int("04", 16):"STM", 
- 			int("20", 16):"LDM", 
- 			int("80", 16):"CMP", 
- 			int("40", 16):"SYSCALL", 
- 			int("02", 16):"STK",
- 			int("01", 16):"JMP"
- 			# int("06", 16):"PUSH", 
- 			# int("07", 16):"POP"
- 		}
 
 jumps = {
 			int("02", 16):"EQL",
@@ -25,20 +13,87 @@ jumps = {
  		}
 
 registers = {
-			int("10", 16):"a",
+			int("20", 16):"a",
  			int("08", 16):"b", 
  			int("04", 16):"c", 
- 			int("20", 16):"d", 
- 			int("80", 16):"s", 
- 			int("40", 16):"i", 
- 			int("02", 16):"f",
+ 			int("10", 16):"d", 
+ 			int("01", 16):"s", 
+ 			int("02", 16):"i", 
+ 			int("90", 16):"f",
+ 			int("00", 16):"none",
 
  		}
 
 syscalls = {
 			int("04", 16):"read",
  			int("10", 16):"write", 
+ 			int("02", 16):"exit",
+ 			int("20", 16):"open",
  		}
+
+
+def mov(arg1, arg2):
+	print("MOV", end=' ')
+	print(registers[arg1], end=' ')
+	print("=", end=' ')
+	print(hex(arg2))
+
+def add(arg1, arg2):
+	print("ADD", end=' ')
+	print(registers[arg1], end=' ')
+	print("+=", end=' ')
+	print(registers[arg2])
+
+def stm(arg1, arg2):
+	print("STM *", end='')
+	print(registers[arg1], end=' ')
+	print("=", end=' ')
+	print(registers[arg2])
+
+def ldm(arg1, arg2):
+	print("LDM", end=' ')
+	print(registers[arg1], end=' ')
+	print("= *", end='')
+	print(registers[arg2])
+
+def cmp(arg1, arg2):
+	print("CMP", end=' ')
+	print(registers[arg1], end=' ')
+	print("?=", end=' ')
+	print(registers[arg2])
+
+def sys(arg1, arg2):
+	print("SYSCALL", end=' ')
+	print(syscalls[arg1], end='')
+	print(",", end=' ')
+	print(registers[arg2])
+
+def stk(arg1, arg2):
+	if (arg1!=0):
+		print("POP", end=' ')
+		print(registers[arg1])
+
+	if (arg2!=0):
+		print("PUSH", end=' ')
+		print(registers[arg2])
+
+def jmp(arg1, arg2):
+	print("JMP", end=' ')
+	print('flg', end='')
+	print(",", end=' ')
+	print(registers[arg2])
+
+opcodes = {
+			int("10", 16): mov,
+ 			int("08", 16): add, 
+ 			int("04", 16): stm, 
+ 			int("20", 16): ldm, 
+ 			int("80", 16): cmp, 
+ 			int("40", 16): sys, 
+ 			int("02", 16): stk,
+ 			int("01", 16): jmp,
+		}
+
 
 f=open("vmcode.txt", "r")
 
@@ -59,9 +114,9 @@ for i in range(0, len(vmcode), 3):
 	vmcode2[i+1] = vmcode[i + kolejnosc.index(2)]
 	vmcode2[i+2] = vmcode[i + kolejnosc.index(3)]
 
-print(vmcode)
-print("\n\n")
-print(vmcode2)
+# print(vmcode)
+# print("\n\n")
+# print(vmcode2)
 vmcode = vmcode2
 
 klucze = opcodes.keys()
@@ -70,6 +125,7 @@ for i in range(0, len(vmcode), 3):
 		print("UNKNOWN")
 		continue
 	# print(hex(vmcode[i]))
-	print(opcodes[vmcode[i]])
+	opcodes[vmcode[i]](vmcode[i+1], vmcode[i+2])
+
 
 
